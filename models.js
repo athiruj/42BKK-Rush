5aa8455df2b8e723b9cd45e5dd329361d4786bd5
Athiruj.K <78010641+athiruj@users.noreply.github.com>
2023-03-28 13:07:50 +0700

    Add files via upload

---
+++ models.js
@@ -0,0 +1,16 @@
+
+//Size Class
+export class Size{
+    constructor(){
+        this.height;
+        this.width;
+    }
+}
+
+//Position Class
+export class Position{
+    constructor(x = 0, y =0){
+        this.x = x;
+        this.y = y;
+    }
+}
---
+++ script.js
@@ -0,0 +1,75 @@
+import * as models from "./models.js";
+
+let colors = ["F45C3B", "FFC300", "009733"];
+let sectionClassname = [];
+let displaySize = new models.Size();
+
+window.addEventListener("resize", () => {
+  displaySize.height = document.body.offsetHeight;
+  displaySize.width = document.body.offsetWidth;
+});
+
+displaySize.height = document.body.offsetHeight;
+displaySize.width = document.body.offsetWidth;
+
+document.body.querySelectorAll("section").forEach(element => {
+  sectionClassname.push(element.id);
+});
+
+class SetElement{
+    constructor(element){
+        this.element =element;
+    }
+
+    rand(max){
+        return Math.floor(Math.random() * max)
+    }
+
+    genColor(colors) {
+      let nColors = [];
+      this.element.forEach(() => {
+        nColors.push(colors[Math.floor(Math.random() * colors.length)]);
+      });
+      return nColors;
+    }
+
+     genPos() {
+      let nPos = [];
+      this.element.forEach(() => {
+        nPos.push(
+          new models.Position(this.rand(displaySize.width), this.rand(displaySize.height))
+        );
+      });
+      return nPos;
+    }
+ 
+    setPos(){
+        let x=0;
+        let e;
+        const nPos = this.genPos()
+        this.element.forEach((cName)=> {
+            e = document.getElementById(cName)
+            e.style.transitionDuration = "3s"
+            e.style.transform = `translate(${nPos[x].x}px,${nPos[x].y}px)`
+            x += 1
+        })
+    }
+
+    setColor(colors){
+        let x=0;
+        let e;
+        const nColor = this.genColor(colors)
+        this.element.forEach((cName)=> {
+            e = document.getElementById(cName)
+            e.style.backgroundColor = `#${nColor[x]}`
+
+            x += 1
+        })
+    }
+
+}
+
+// console.log(genPos(sectionClassname));
+const btn = document.getElementById("boxA")
+btn.addEventListener("mouseover",()=> new SetElement(sectionClassname).setColor(colors))
+btn.addEventListener("mouseover",()=> new SetElement(sectionClassname).setPos())
